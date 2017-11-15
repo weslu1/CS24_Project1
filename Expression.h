ee3f44c376711f102d736cfe615b8b290b8a74bd
alfonzzo42 <33643373+alfonzzo42@users.noreply.github.com>
2017-11-14 23:38:32 -0800

    Create Expression.h

---
+++ Expression.h
@@ -0,0 +1,12 @@
+#ifndef EXPRESSION_H_
+#define EXPRESSION_H_
+
+class Expression{
+public:
+	String infixString();
+	String prefixString(node n);
+	String postfixString();
+
+}
+
+#endif /* EXPRESSION_H_ */
